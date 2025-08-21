b48abf9f3132cbb535bb1fb98700d07f98914523
Pothina Chandrikarani <140737438+ChanduCharanSample@users.noreply.github.com>
2025-08-21 19:15:53 +0530

    Update cloudcupcake.sh

--- Cloud SQL with Terraform/cloudcupcake.sh
+++ Cloud SQL with Terraform/cloudcupcake.sh
@@ -1,15 +1,7 @@
-gcloud auth list
-
-export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
-
-export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
-
-export PROJECT_ID=$(gcloud config get-value project)
-
-gcloud config set compute/zone "$ZONE"
-
-gcloud config set compute/region "$REGION"
-
-gcloud sql instances create myinstance --project=$DEVSHELL_PROJECT_ID --region=$REGION --root-password=techcps --tier=db-n1-standard-4 --database-version=MYSQL_8_0
-
-gcloud sql databases create guestbook --instance=myinstance
+mkdir sql-with-terraform
+cd sql-with-terraform
+gsutil cp -r gs://spls/gsp234/gsp234.zip .
+unzip gsp234.zip
+terraform init
+terraform plan -out=tfplan
+terraform apply tfplan
