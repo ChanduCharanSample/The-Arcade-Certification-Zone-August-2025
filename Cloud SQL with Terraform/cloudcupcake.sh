bd77c036a41008729eef7c2e5e5851dcfeb4950e
Pothina Chandrikarani <140737438+ChanduCharanSample@users.noreply.github.com>
2025-08-21 19:42:08 +0530

    Update cloudcupcake.sh

--- Cloud SQL with Terraform/cloudcupcake.sh
+++ Cloud SQL with Terraform/cloudcupcake.sh
@@ -2,13 +2,13 @@
 mkdir -p sql-with-terraform
 cd sql-with-terraform
 
-# 2. Download Terraform files from Cloud Storage
+# 2. Download Terraform files
 gsutil cp -r gs://spls/gsp234/gsp234.zip .
 
 # 3. Unzip contents
 unzip -o gsp234.zip
 
-# 4. Update variables.tf with lab project and region
+# 4. Update variables.tf with your project ID and allowed region
 sed -i 's/project *=.*/project = "qwiklabs-gcp-03-c86dd09f3ca7"/' variables.tf
 sed -i 's/region *=.*/region = "us-east1"/' variables.tf
 
@@ -18,5 +18,5 @@ terraform init
 # 6. Create execution plan
 terraform plan -out=tfplan
 
-# 7. Apply configuration to create Cloud SQL instance
+# 7. Apply configuration (auto-approve)
 terraform apply -auto-approve tfplan
